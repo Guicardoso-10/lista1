71dd08a79f734536990b4aefa74decb64894292e
Guicardoso-10 <guiguicardoso-10@hotmail.com>
2022-08-11 20:39:07 -0300

    2/7

--- lista1.js
+++ lista1.js
@@ -1,1 +1,9 @@
 console.log ("Alô mundo!")
+//Exercicio 2
+import {questionInt} from "readline-sync"
+let nota1 = questionInt ('digite a nota 1:')
+let nota2 = questionInt ('digite a nota2:')
+let nota3 = questionInt ('digite a nota3:')
+let nota4 = questionInt ('digite a nota 4:')
+let média = "sua média é:" + (nota1 + nota2 + nota3 + nota4)/4
+console.log (média)
